05f6b13f213c22f33b8e229fe3c74145de919e33
Ram Chimata <rchimata@rythmos.com>
2022-10-02 11:53:45 +0530

    Added docker file

---
+++ src/main/java/com/learn/ram/kafka/DeviceProducer.java
@@ -0,0 +1,8 @@
+package com.learn.ram.kafka;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DeviceProducer {
+
+}
--- src/test/java/com/example/demo/MyKafkaApplicationTests.java
+++ src/test/java/com/example/demo/MyKafkaApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MyKafkaApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+
 
 }
